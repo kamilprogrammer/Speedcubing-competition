f2bf061dd8c18ca9a29b1e53d21ca659160bd35e
Kamel Rifai <kamilref00@gmail.com>
2025-07-26 23:56:40 +0300

    Added a preload and also some simple changes.

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -28,8 +28,6 @@ export default function Home() {
         <CustomCursor />
         <HeroSection />
         <AnimatedTestimonials testimonials={testimonials} />
-        {/* 
-        <Cube />*/}
         <Cta />
       </main>
     </div>
